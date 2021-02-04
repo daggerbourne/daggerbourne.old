188c9aa05de27e3a924af9617f2894b653c4a5a6
Valentino <valentino@dagherfam.com>
2021-02-03 21:53:49 -0600

    coppied etch a sketch over

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const gridSize = 256
+const color = 'red'
+
+//Div Generator
+function divGenerate(gridSize){
+for(let i = 0; i < gridSize; i++){
+    let gridItems = document.createElement('div');
+    gridItems.classList.add('gridItem')
+    container.appendChild(gridItems);
+    console.log('genreated')
+}}
+
+//Generate Divs
+divGenerate(gridSize);
+
+
+const gridItems = document.querySelectorAll('.gridItem');
+
+function changeDivColor(color){
+    console.log(`Color Changed to ${color}`)
+     this.style.backgroundColor = "red";
+}
+
+gridItems.forEach(function(gridItem){
+    console.log('Binding gridItem');
+    gridItem.addEventListener('mouseover', changeDivColor)
+})
+
+
+// gridItems.forEach(function (gridItem){
+//     gridItem.addEventListener('click', changeDivColor);
+
+// })
